bc887981aeef9bfb3593adaecb8290ed7a50fe55
Vikttria <viktoria.gargya@gmail.com>
2020-01-08 21:35:09 +0100

    spring project letrehozasa

---
+++ zalog_program/src/main/java/hu/vikttria/zalog_program/ZalogProgramApplication.java
@@ -0,0 +1,13 @@
+package hu.vikttria.zalog_program;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZalogProgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZalogProgramApplication.class, args);
+	}
+
+}
---
+++ zalog_program/src/test/java/hu/vikttria/zalog_program/ZalogProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.vikttria.zalog_program;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZalogProgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
