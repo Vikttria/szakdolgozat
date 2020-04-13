78b6d54ab46bd85250503b58ec226c954c5dbeb5
Vikttria <viktoria.gargya@gmail.com>
2020-04-13 19:01:32 +0200

    adding front end files

--- zalog_program/src/main/java/hu/vikttria/zalog_program/controller/HomeController.java
+++ zalog_program/src/main/java/hu/vikttria/zalog_program/controller/HomeController.java
@@ -1,6 +1,5 @@
 package hu.vikttria.zalog_program.controller;
 
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,7 +8,32 @@ public class HomeController {
 
     @RequestMapping("/")
     public String home(){
+        return "bejelentkezes";
+    }
+
+    @RequestMapping("/felvet")
+    public String felvet(){
         return "felvet";
+    }
+
+    @RequestMapping("/kivalt")
+    public String kivalt(){
+        return "kivalt";
+    }
+
+    @RequestMapping("/hosszabbit")
+    public String hosszabbit(){
+        return "hosszabbit";
+    }
+
+    @RequestMapping("/kereses")
+    public String kereses(){
+        return "kereses";
+    }
+
+    @RequestMapping("/bejelentkezes")
+    public String bejelentkezes(){
+        return "bejelentkezes";
     }
 
 }
