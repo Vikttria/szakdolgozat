2445d80188089c2c5976f6146ec589ae804857b0
Vikttria <viktoria.gargya@gmail.com>
2020-05-11 13:36:34 +0200

    role

--- zalog_program/src/main/java/hu/vikttria/zalog_program/zaloghaz/Role.java
+++ zalog_program/src/main/java/hu/vikttria/zalog_program/zaloghaz/Role.java
@@ -15,11 +15,10 @@ public class Role {
     @ManyToMany(mappedBy = "roles")
     private Set<User> users;
 
-
     public Role() {
     }
 
-    public Role(String name) {
+    public Role(String name){
         this.name = name;
     }
 
